9c8890cce5c3d92bae859b8dacb9258f4b78d711
Koen <koen@koenvh.nl>
2025-05-11 21:37:11 +0200

    Update logo

--- PedalPal/Main.Designer.cs
+++ PedalPal/Main.Designer.cs
@@ -124,7 +124,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::PedalPal.Properties.Resources.gas_pedal;
             this.pictureBox1.Location = new System.Drawing.Point(12, 27);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(61, 56);
